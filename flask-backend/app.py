ea19d270c9e7940d6a664aee93d8512fa2af2d47
Swizec Teller <swizec@swizec.com>
2025-03-22 17:37:52 -0700

    Use composable views

--- flask-backend/app.py
+++ flask-backend/app.py
@@ -3,16 +3,20 @@ import requests
 
 app = Flask(__name__)
 
+@app.route("/", methods=["GET"])
+def hello_world():
+    return render_template('hello.html', views=dict(cat=cat,))
+
 def get_random_cat():
     req = requests.get('https://api.thecatapi.com/v1/images/search')
     url = req.json()[0]['url']
     
     return url
 
-@app.route("/", methods=["GET"])
-def hello_world():
+@app.route('/cat', methods=['GET'])
+def cat():
     cat_url = get_random_cat()
-    return render_template('hello.html', cat_url=cat_url)
+    return render_template('cat.html', cat_url=cat_url)
 
 if __name__ == "__main__":
     app.run(debug=True, port=8000)
